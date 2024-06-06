e8ff1e006bd4671c2bff71d2824af19841fcbf59
geraldkisilu <muligerald6@gmail.com>
2024-06-06 11:51:13 +0300

    commit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, page_count):
+        self.title = title
+        if isinstance(page_count, int):
+            self.page_count = page_count
+        else:
+            print("page_count must be an integer")
+            self.page_count = None
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
     pass
     
         
